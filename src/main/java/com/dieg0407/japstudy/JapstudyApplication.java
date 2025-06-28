271505aa5a5446a0141627e9db96d3662b5babc4
Dieg0407 <diego.pastor.0407@gmail.com>
2025-06-27 20:24:23 -0500

    chore: initial commit

---
+++ src/main/java/com/dieg0407/japstudy/JapstudyApplication.java
@@ -0,0 +1,13 @@
+package com.dieg0407.japstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dieg0407/japstudy/JapstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieg0407.japstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
